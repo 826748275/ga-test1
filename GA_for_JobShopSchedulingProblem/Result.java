665a0ae2d6ae8c0d2eb24c3520fc576eb4dfce8e
lzy <826748275@qq.com>
2022-05-28 16:33:31 +0800

    push ga chongtu commit

--- GA_for_JobShopSchedulingProblem/Result.java
+++ GA_for_JobShopSchedulingProblem/Result.java
@@ -12,5 +12,6 @@ class Result {
         System.out.println("merge·ÖÖ§");
         System.out.println("³åÍ»master");
         System.out.println("³åÍ»hot-fix");
+        System.out.println("pushµ½github");
     }
 }
